5b14b81323d5b1709bacbee6817c47628abe738a
sinasadeghi83 <sina.sadeghi83@gmail.com>
2023-05-08 19:49:30 +0330

    Add Rule Component

---
+++ src/Components/Rule.java
@@ -0,0 +1,15 @@
+package Components;
+
+import java.util.HashMap;
+
+public abstract class Rule {
+    /**
+     * Thanks to Yii framework developers for PHP for giving me these notions and ideas
+     * https://www.yiiframework.com/doc/guide/2.0/en/security-authorization
+     * @param auth Auth the user object.
+     * @param item String the role or permission that this rule is associated with
+     * @param params HashMap parameters passed to.
+     * @return bool a value indicating whether the rule permits the role or permission it is associated with.
+     */
+    public abstract boolean execute(Auth auth, String item, HashMap<String, Object> params);
+}
