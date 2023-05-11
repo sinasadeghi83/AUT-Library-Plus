e9b6da1734d7c2a79d6c2b2b220dc272629a552a
sinasadeghi83 <sina.sadeghi83@gmail.com>
2023-05-11 13:51:48 +0330

    Add add-selling-book to ResourceController and change checking for uniqueness of resources

--- src/Controllers/ResourceController.java
+++ src/Controllers/ResourceController.java
@@ -1,8 +1,10 @@
 package Controllers;
 
+import Components.Model;
 import Components.Response;
 import Main.App;
 import Models.Book;
+import Models.SellingBook;
 import Models.Thesis;
 
 import java.util.List;
@@ -13,7 +15,8 @@ public class ResourceController extends BaseController{
     public Map<String, List<String>> accessControl() {
         return Map.of(
                 "actionAddBook", List.of("manager"),
-                "actionAddThesis", List.of("manager")
+                "actionAddThesis", List.of("manager"),
+                "actionAddSellingBook", List.of("manager")
         );
     }
 
@@ -21,7 +24,7 @@ public class ResourceController extends BaseController{
         Book book = new Book(args.get(0), args.get(1), args.get(2), args.get(3), args.get(4), Integer.parseInt(args.get(5)), args.get(6), args.get(7));
         if(!book.validate()){
             String zeroError = (String) book.getErrors().values().toArray()[0];
-            if(zeroError.equals(Book.UNIQUE_LIBRARY_ERR)){
+            if(zeroError.equals(Book.UNIQUE_LIBRARY_ERR) || zeroError.equals(Model.UNIQUE_ERR)){
                 return new Response(1); //duplicate-id
             }
             return new Response(2); //not-found
@@ -37,7 +40,7 @@ public class ResourceController extends BaseController{
         Thesis thesis = new Thesis(args.get(0), args.get(1), args.get(2), args.get(3), args.get(4), args.get(5), args.get(6));
         if(!thesis.validate()){
             String zeroError = (String) thesis.getErrors().values().toArray()[0];
-            if(zeroError.equals(Book.UNIQUE_LIBRARY_ERR)){
+            if(zeroError.equals(Book.UNIQUE_LIBRARY_ERR) || zeroError.equals(Model.UNIQUE_ERR)){
                 return new Response(1); //duplicate-id
             }
             return new Response(2); //not-found
@@ -46,6 +49,22 @@ public class ResourceController extends BaseController{
             return new Response(5); //permission-denied
         }
         thesis.save();
+        return new Response(0); //success
+    }
+
+    public Response actionAddSellingBook(List<String> args){
+        SellingBook book = new SellingBook(args.get(0), args.get(1), args.get(2), args.get(3), args.get(4), Integer.parseInt(args.get(5)), Integer.parseInt(args.get(6)), Integer.parseInt(args.get(7)), args.get(8), args.get(9));
+        if(!book.validate()){
+            String zeroError = (String) book.getErrors().values().toArray()[0];
+            if(zeroError.equals(SellingBook.UNIQUE_LIBRARY_ERR) || zeroError.equals(Model.UNIQUE_ERR)){
+                return new Response(1); //duplicate-id
+            }
+            return new Response(2); //not-found
+        }
+        if(!App.getAuthManager().can("addResource", Map.of("libraryId", book.getLibId()))){
+            return new Response(5); //permission-denied
+        }
+        book.save();
         return new Response(0); //success
     }
 
