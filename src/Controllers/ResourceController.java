af1954c92e45e2dff413db25e7eedd9dd3a87dd1
sinasadeghi83 <sina.sadeghi83@gmail.com>
2023-05-11 14:00:53 +0330

    Add check permission-denied before duplicate-id in ResourceController

--- src/Controllers/ResourceController.java
+++ src/Controllers/ResourceController.java
@@ -22,48 +22,51 @@ public class ResourceController extends BaseController{
 
     public Response actionAddBook(List<String> args){
         Book book = new Book(args.get(0), args.get(1), args.get(2), args.get(3), args.get(4), Integer.parseInt(args.get(5)), args.get(6), args.get(7));
+        boolean userCan = App.getAuthManager().can("addResource", Map.of("libraryId", book.getLibId()));
         if(!book.validate()){
             String zeroError = (String) book.getErrors().values().toArray()[0];
-            if(zeroError.equals(Book.UNIQUE_LIBRARY_ERR) || zeroError.equals(Model.UNIQUE_ERR)){
+            boolean uniqueErr = zeroError.equals(SellingBook.UNIQUE_LIBRARY_ERR) || zeroError.equals(Model.UNIQUE_ERR);
+            if(userCan && uniqueErr){
                 return new Response(1); //duplicate-id
-            }
-            return new Response(2); //not-found
+            }else if(!uniqueErr)
+                return new Response(2); //not-found
         }
-        if(!App.getAuthManager().can("addResource", Map.of("libraryId", book.getLibId()))){
+        if(!userCan)
             return new Response(5); //permission-denied
-        }
         book.save();
         return new Response(0); //success
     }
 
     public Response actionAddThesis(List<String> args){
         Thesis thesis = new Thesis(args.get(0), args.get(1), args.get(2), args.get(3), args.get(4), args.get(5), args.get(6));
+        boolean userCan = App.getAuthManager().can("addResource", Map.of("libraryId", thesis.getLibId()));
         if(!thesis.validate()){
             String zeroError = (String) thesis.getErrors().values().toArray()[0];
-            if(zeroError.equals(Book.UNIQUE_LIBRARY_ERR) || zeroError.equals(Model.UNIQUE_ERR)){
+            boolean uniqueErr = zeroError.equals(SellingBook.UNIQUE_LIBRARY_ERR) || zeroError.equals(Model.UNIQUE_ERR);
+            if(userCan && uniqueErr){
                 return new Response(1); //duplicate-id
-            }
-            return new Response(2); //not-found
+            }else if(!uniqueErr)
+                return new Response(2); //not-found
         }
-        if(!App.getAuthManager().can("addResource", Map.of("libraryId", thesis.getLibId()))){
+        if(!userCan)
             return new Response(5); //permission-denied
-        }
         thesis.save();
         return new Response(0); //success
     }
 
     public Response actionAddSellingBook(List<String> args){
         SellingBook book = new SellingBook(args.get(0), args.get(1), args.get(2), args.get(3), args.get(4), Integer.parseInt(args.get(5)), Integer.parseInt(args.get(6)), Integer.parseInt(args.get(7)), args.get(8), args.get(9));
+        boolean userCan = App.getAuthManager().can("addResource", Map.of("libraryId", book.getLibId()));
         if(!book.validate()){
             String zeroError = (String) book.getErrors().values().toArray()[0];
-            if(zeroError.equals(SellingBook.UNIQUE_LIBRARY_ERR) || zeroError.equals(Model.UNIQUE_ERR)){
+            boolean uniqueErr = zeroError.equals(SellingBook.UNIQUE_LIBRARY_ERR) || zeroError.equals(Model.UNIQUE_ERR);
+            if(userCan && uniqueErr){
                 return new Response(1); //duplicate-id
-            }
-            return new Response(2); //not-found
+            }else if(!uniqueErr)
+                return new Response(2); //not-found
         }
-        if(!App.getAuthManager().can("addResource", Map.of("libraryId", book.getLibId()))){
+        if(!userCan)
             return new Response(5); //permission-denied
-        }
         book.save();
         return new Response(0); //success
     }
